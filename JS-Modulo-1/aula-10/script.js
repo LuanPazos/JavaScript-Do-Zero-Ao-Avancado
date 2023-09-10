4ffc7304e17469b0f17a30287dbf5d67f9b55de8
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-10 09:03:06 -0300

    Create aula 10

---
+++ JS-Modulo-1/aula-10/script.js
@@ -0,0 +1,13 @@
+let nome= 'luan'
+
+console.log(nome)
+// console.error(nome)
+// console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt('insira o seu nome')
+
+// let confirmacao = window.confirm('conrfirma essa ação?')
+// console.log(confirmacao) // retorna boolean
+
+document.getElementById

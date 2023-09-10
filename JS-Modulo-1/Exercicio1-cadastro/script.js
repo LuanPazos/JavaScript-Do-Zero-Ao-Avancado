882d2fc162f7cabcee5a611259d5b9def30fc785
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-10 09:13:15 -0300

    Update script.js
    uso do document.write

--- JS-Modulo-1/Exercicio1-cadastro/script.js
+++ JS-Modulo-1/Exercicio1-cadastro/script.js
@@ -1,14 +1,13 @@
 const nome= window.prompt('Diga o seu nome')
-
 const sobrenome= window.prompt('Diga o seu sobrenome')
-
 let campoDeEstudo = window.prompt('diga o campo de estudo')
-
-
 const anoDeNascimento = window.prompt('diga o ano de nascimento')
+
 parseFloat(anoDeNascimento)
 let idade = 2023 - anoDeNascimento 
-console.log(idade)
 
-console.log(nome +' '+ sobrenome,campoDeEstudo, idade)
+console.log(nome +' '+ sobrenome,campoDeEstudo, idade +' ')
+document.write('Nome: '+nome +' '+ sobrenome+' ')
+document.write('Campo de Estudo: '+ campoDeEstudo+' ')
+document.write('idade: '+ idade)
 

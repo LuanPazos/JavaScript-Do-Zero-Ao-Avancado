9b4902bc20bce3629f8befa22fa4d2fca5658cdf
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-10 12:02:34 -0300

    Create Aula 16 if and else

---
+++ JS-Modulo-1/aula16-IF-ELSE/script.js
@@ -0,0 +1,13 @@
+const idade = prompt('informe a sua idade')
+
+
+if(idade > 18){
+  alert('voce é maior de idade')
+} else if(idade>12){
+  alert('voce é menor de idade')
+} else if(idade>4){
+  alert('voce é criança')
+} else{
+  alert('..')
+}
+

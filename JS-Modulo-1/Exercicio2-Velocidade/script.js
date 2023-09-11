6da50409bd4eb26977dfdb7efe53673eae6917f3
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-11 15:27:04 -0300

    Update script.js
    uso do document.write no lugar do alert

--- JS-Modulo-1/Exercicio2-Velocidade/script.js
+++ JS-Modulo-1/Exercicio2-Velocidade/script.js
@@ -4,9 +4,9 @@ let velocidade2 = window.prompt('Velocidade do carro 2')
 let vel2 = parseFloat(velocidade2)
 
 if (vel1 > vel2) {
-  alert('Velocidade do carro 1 é maior que a do carro 2')
+  document.write('Velocidade do carro 1(' + vel1 +') é maior que a do carro 2 (' + vel2 +')')
 } else if (vel2 > vel1) {
-  alert('Velocidade do carro 2 é maior que a do carro 1')
+  document.write('Velocidade do carro 2(' + vel2 +') é maior que a do carro 1 (' + vel1 +')')
 } else {
-  alert('As velocidades são iguais')
+  document.write('As velocidades são iguais ' + vel1 + ' e ' + vel2)
 }

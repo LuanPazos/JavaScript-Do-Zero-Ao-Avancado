20e21719f0b207df559c658aab7856b219b55aa9
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-13 22:47:34 -0300

    Update script.js
    For

--- JS-Modulo-1/For/script.js
+++ JS-Modulo-1/For/script.js
@@ -1,8 +1,6 @@
 // inicialização ; condição ; finalização
-let indice = 0
-for (
-  alert('inicializando o for');indice <=3;alert('finalizando o for'))
-  {
-  alert(indice)
-  indice++
+let nome = 'luan machado'
+
+for(let indice = 0; indice<nome.length; indice++){
+  console.log(nome[indice])
 }

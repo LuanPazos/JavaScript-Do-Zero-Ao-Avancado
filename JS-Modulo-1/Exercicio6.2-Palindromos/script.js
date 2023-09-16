81db8c90a488e92a90343b2031e1c27289cd933f
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-15 23:01:30 -0300

    Update script.js

--- JS-Modulo-1/Exercicio6.2-Palindromos/script.js
+++ JS-Modulo-1/Exercicio6.2-Palindromos/script.js
@@ -4,3 +4,9 @@ let palavraInvertida = ""
 for (let i = palavra.length - 1; i >= 0; i--) {
   palavraInvertida += palavra[i]
 }
+
+if (palavra === palavraInvertida) {
+  alert(palavra + " é um palíndromo!")
+} else {
+  alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida)
+}

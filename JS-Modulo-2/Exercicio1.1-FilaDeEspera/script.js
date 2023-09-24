522b742ed75d6b899d331e469f8c6832f3f76dd5
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-24 19:47:01 -0300

    Update script.js

--- JS-Modulo-2/Exercicio1.1-FilaDeEspera/script.js
+++ JS-Modulo-2/Exercicio1.1-FilaDeEspera/script.js
@@ -2,5 +2,34 @@ let fila = []
 let opcao = ""
 
 do {
-  
+  let pacientes = ""
+  for (let i = 0; i < fila.length; i++) {
+    pacientes += (i + 1) + "º - " + fila[i] + "\n"
+  }
+
+  opcao = prompt(
+    "Pacientes:\n" + pacientes +
+    "\nEscolha uma ação:\n1 - Novo paciente\n2 - Consultar paciente\n3 - Sair"
+  )
+
+  switch (opcao) {
+    case "1":
+      const novoPaciente = prompt("Qual é o nome do paciente?")
+      fila.push(novoPaciente)
+      break
+    case "2":
+      const pacienteConsultado = fila.shift()
+      if (!pacienteConsultado) {
+        alert("Não há pacientes na fila!")
+      } else {
+        alert(pacienteConsultado + " foi removido da fila.")
+      }
+      break
+    case "3":
+      alert("Encerrando...")
+      break
+    default:
+      alert("Opção inválida!")
+      break
+  }
 } while (opcao !== "3");

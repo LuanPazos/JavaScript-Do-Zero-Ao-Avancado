0bbe67be9344ebe45cc6079bf0412cc7573902a1
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-27 05:38:46 -0300

    Update matriz.js

--- JS-Modulo-2/aula-Matriz/matriz.js
+++ JS-Modulo-2/aula-Matriz/matriz.js
@@ -19,3 +19,11 @@ console.log(matriz[1][1])
 console.log(matriz[2])
 console.log(matriz[2][0])
 console.log(matriz[2][1][0])
+
+// Podemos ver claramente a estrutura de uma matriz em uma tabela
+const matriz2 = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+  ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+  ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+]
+console.table(matriz2)

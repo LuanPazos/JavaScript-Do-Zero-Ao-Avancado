27cb201f5a6320bd6dd3f52c227660f4786380b9
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-09-27 04:28:01 -0300

    Create script.js

---
+++ JS-Modulo-2/Exercicio1.2-Baralho/script.js
@@ -0,0 +1,36 @@
+let quantidade = 0
+let opcao = 0
+let pilha = []
+let novaCarta= ''
+let cartaRemovida = ''
+
+do{
+
+  opcao = Number(prompt(
+    'O baralho tem ' + quantidade + ' cartas: \n' +
+    pilha +
+    '\nEscolha uma opcao:'+
+    '\n1- adicionar uma carta' +
+    '\n2- Pegar uma carta'+
+    '\n3- Encerrar'
+  ))
+  switch (opcao){
+    case 1:
+      novaCarta= prompt('Digite uma carta para adicionar ao topo do baralho: ')
+      pilha.unshift(novaCarta)
+      quantidade ++
+      break
+    case 2:
+      cartaRemovida = pilha.shift()
+      alert('a carta: '+ cartaRemovida + ' foi removida')
+      quantidade --
+      break
+    case 3:
+      alert('Encerrando...')
+      break
+    default:
+      alert('Opção invalida')
+      break
+  }
+  
+} while(opcao!=3)

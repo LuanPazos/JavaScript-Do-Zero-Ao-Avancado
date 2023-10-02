983eeac37b4393e375253054e193c7e1c75855fc
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-10-02 09:51:59 -0300

    Update script.js

--- JS-Modulo-2/Exercicio10-SistemaDeVagas/script.js
+++ JS-Modulo-2/Exercicio10-SistemaDeVagas/script.js
@@ -1,57 +1,120 @@
-function menu(){
-  const opcao = prompt(`====== Vagas de Emprego ======
+const vagas = []
+
+function menu() {
+  const option = Number(prompt(`
+  ====== Vagas de Emprego ======
   1. Listar vagas
   2. Cadastrar nova vaga
   3. Visualizar vaga
   4. Candidatar-se a uma vaga
   5. Excluir vaga
   6. Sair
-  `)
-  return opcao
+  `))
+  return option
 }
 
-let vagas = []
-function listarVagas(){
+function listarVagas() {
   const vagasEmTexto = vagas.reduce((textoFinal, vaga, index) => {
-    textoFinal += indice + '.'
+    textoFinal += index + '.'
     textoFinal += vaga.nome
-    textoFinal += '(' + vaga.candidato + 'candidatos)\n'
+    textoFinal += ' (' + vaga.candidatos.length + ' candidatos)\n'
     return textoFinal
   }, '')
-  alert (vagasEmTexto)
+  alert(vagasEmTexto)
 }
 
+function cadastrarVaga() {
+  const nome = prompt(`Digite o nome da vaga`)
+  const descricao = prompt(`Digite a descricao da vaga`)
+  const dataLimite = prompt(`Digite a data limite da vaga`)
+  const confirmacao = confirm(`Confirma a criacao da vaga?\n Nome ${nome}\n Descrição: ${descricao}\n Data limite: ${dataLimite}`)
 
+  if (confirmacao) {
+    const novaVaga = {
+      nome,
+      descricao,
+      dataLimite,
+      candidatos: []
+    }
+    vagas.push(novaVaga)
+    alert(`Vaga criada com sucesso`)
+  }
+}
 
+function exibirVaga() {
+  const index = Number(prompt(`Digite o indice da vaga`))
+  const vaga = vagas[index]
+  // usando reduce
+  const candiatosEmtexto = vaga.candidatos.reduce((textoFinal, candidato) => textoFinal + candidato,'')
+  alert(
+    'vaga Numero: ' + index +
+    '\nNome: ' + vaga.nome +
+    '\nDescricao: ' + vaga.descricao +
+    '\nData limite: ' + vaga.dataLimite +
+    '\nQuantidade de candidatos: ' + vaga.candidatos.length +
+    '\nCandidatos: ' + candiatosEmtexto
+  )
+}
 
+function inscreverCandidato() {
+  const index = Number(prompt(`Digite o indice da vaga`))
+  const vaga = vagas[index]
+  const nome = prompt(`Digite o nome do candidato`)
+  const confirmacao = confirm(`Confirma a inscricao do candidato ${nome} na vaga ${vaga.nome}?`)
 
-let option = 0
-function programa(){
-  do{
-    switch(option){
+  if (confirmacao) {
+    vaga.candidatos.push(nome)
+    alert(`Candidato inscrito com sucesso`)
+  } else {
+    alert(`Operacao cancelada`)
+  }
+}
+
+function excluirVaga() {
+  const index = Number(prompt(`Digite o indice da vaga`))
+  const vaga = vagas[index]
+  const confirmacao = confirm(`Confirma a exclusao da vaga ${vaga.nome}?`)
+
+  if (confirmacao) {
+    vagas.splice(index, 1)
+    alert(`Vaga excluida com sucesso`)
+  } else {
+    alert(`Operacao cancelada`)
+  }
+}
+
+function sair() {
+  alert(`Obrigado por usar o sistema de vagas\n Volte sempre!`)
+}
+
+function programa() {
+  let option = 0
+  do {
+    option = Number(menu())
+    switch (option) {
       case 1:
         listarVagas()
         break
       case 2:
-        cadastrarVagas()
+        cadastrarVaga()
         break
       case 3:
-        visualizarVaga()
+        exibirVaga()
         break
       case 4:
-        candidatarVaga()
+        inscreverCandidato()
         break
       case 5:
         excluirVaga()
         break
       case 6:
-        console.log(`Obrigado por usar o sistema de vagas\n Volte sempre!`)
+        sair()
         break
       default:
         console.log(`Opcao invalida`)
         break
     }
-  }while(option != 6)
+  } while (option !== 6)
 }
 
 programa()

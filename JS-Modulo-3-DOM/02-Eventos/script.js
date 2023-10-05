34703714cd0ff7a0c0cac04e3c97655863fd3e67
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-10-05 16:23:36 -0300

    Change Folder Name

---
+++ JS-Modulo-3-DOM/02-Eventos/script.js
@@ -0,0 +1,20 @@
+function onMouseEnter() {
+  console.log("Entrou")
+}
+
+function onChange() {
+  console.log("mudança no valor")
+}
+
+function onFocus() {
+  console.log("entrada de foco")
+}
+
+function onBlur() {
+  console.log("perda de foco")
+}
+
+function onClick() {
+  alert('Clicou') 
+}
+

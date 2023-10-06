a94b8edd5fdbc0b8ff082a7f1895f38a24b1ebb9
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-10-06 18:56:16 -0300

    Exercicio 11 - Escalação de time
    function addPlayer

---
+++ JS-Modulo-3-DOM/Exercicio11-Escalação de times/script.js
@@ -0,0 +1,23 @@
+function addPlayer() {
+  const position = document.getElementById('position').value
+  const name = document.getElementById('name').value
+  const number = document.getElementById('number').value
+
+  const confirmation = confirm(`Escalar ${name} como ${position}?`)
+
+  if (confirmation) {
+    const teamList = document.getElementById('team-list')
+    const playerItem = document.createElement('li')
+    playerItem.id = 'player-' + number
+    playerItem.innerText = `${position}: ${name} (${number})`
+    teamList.appendChild(playerItem)
+
+    // zerando os valores das caixas de input:
+    document.getElementById('position').value = ''
+    document.getElementById('name').value = ''
+    document.getElementById('number').value = ''
+
+  } else {
+    alert(`Operação cancelada`)
+  }
+}
